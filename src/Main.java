3ad03c88a9303095a2d079d23c1a10b7749e30ea
GalaxyNova7 <sarmad_nmj@hotmail.com>
2025-03-10 21:09:23 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 50 , b = 40;
+        double c = 15.5 , d = 25.5;
+
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(c*d);
+        System.out.println(a/c);
+    }
+}
